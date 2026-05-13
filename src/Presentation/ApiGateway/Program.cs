0f60c49de48f0eb96749ba92e9fa1e73186df114
Li <820208235@qq.com>
2026-05-13 19:50:40 +0800

    调整架构，BuildingBlocks新增抽象层

--- src/Presentation/ApiGateway/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ApiGateway;
-
-internal class Program
-{
-}
---
+++ tests/Tests.Architecture/Tests.Architecture/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Tests.Architecture;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
